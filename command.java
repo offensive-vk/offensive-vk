d892f926ffe1226054df9535c5b86326bd76f35e
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-03-31 20:26:54 +0530

    Some Common Java Programs
    Nothing to worry about.

---
+++ command.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class command{
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int res = a + b;
+        System.out.println(+res);
+    }
+}
