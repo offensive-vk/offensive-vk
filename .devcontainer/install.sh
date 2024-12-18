471d5bfa0f81a38061cd8f3419184b336fbfa8b1
Vedansh <superuser.ntsystems@outlook.com>
2024-12-18 21:56:36 +0530

    Minor changes.

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -6,16 +6,10 @@ sleep 3
 curl -fsSL https://fnm.vercel.app/install | bash
 
 # activate fnm
+(
 source ~/.bashrc
-
-# download and install Node.js
 fnm use --install-if-missing 22
-
-# verifies the right Node.js version is in the environment
-node -v # should print `v22.11.0`
-
-# verifies the right npm version is in the environment
-npm -v # should print `10.9.0`
-
-# check pnpm
+node -v
+npm -v 
 npx pnpm -v
+) &
